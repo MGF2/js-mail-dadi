67ccc48762cf644f98f9686c527f7a2822d29120
Unknown <fioremartina@hotmail.com>
2020-07-08 16:20:16 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 var email = prompt("Inserisci la tua e-mail") ;
 var lista = ["email@gmail.com", "email1@gmail.com","email2@gmail.com","email3@gmail.com","email4@gmail.com","email5@gmail.com","email6@gmail.com"];
-
+var trovata = false;
 
 for (var i = 0; i < lista.length; i++) {
   if (lista[i] == email) {
-    alert("Accesso consentito")
-  }
-  else {
-    alert("Accesso negato");
+
+    trovata = true;
+
   }
 
+}
+
+if ( trovata == true ) {
+  alert("Mail trovata! Accesso consentito");
+} else {
+  alert("Mail non trovata!")
 }
