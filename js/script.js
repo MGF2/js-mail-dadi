b311efda1c47168e8f0eca6f59bf387382cf899c
Unknown <fioremartina@hotmail.com>
2020-07-09 15:24:00 +0200

    bonus con due dadi

--- js/script.js
+++ js/script.js
@@ -14,3 +14,25 @@ if (giocatore > computer) {
 document.getElementById("vincitore") .innerHTML = vincitore;
 document.getElementById("tu") .innerHTML = "Tu:" + " " + giocatore;
 document.getElementById("pc") .innerHTML = "Computer:" + " " + computer;
+
+//CON DUE DADI
+var gdado1 = Math.floor(Math.random()*6)+1;
+var gdado2 = Math.floor(Math.random()*6)+1;
+var giocatore = gdado1+gdado2;
+var cdado1 = Math.floor(Math.random()*6)+1;
+var cdado2 = Math.floor(Math.random()*6)+1;
+var computer = cdado1+cdado2;
+var vincitore;
+
+
+if (giocatore > computer) {
+  vincitore =  "Hai vinto!";
+} else if (giocatore == computer) {
+  vincitore =  "Pareggio!";
+} else {
+  vincitore =  "Hai perso!";
+}
+
+document.getElementById("vincitore2") .innerHTML = vincitore;
+document.getElementById("tu2") .innerHTML = "Tu:" + " " + gdado1 + " , " + gdado2 + " = " + giocatore;
+document.getElementById("pc2") .innerHTML = "Computer:" + " " + cdado1 + " , " + cdado2 + " = " + computer;
